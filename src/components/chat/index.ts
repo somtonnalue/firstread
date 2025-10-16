0461f6891058662606d050e3677f988b8b56c282
somtonnalue <leroynnalue65@gmail.com>
2025-10-16 21:51:01 +0100

    chore: clean up unused components

--- src/components/chat/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Chat components barrel export
- */
-
-export { ChatHeader } from "./chat-header";
-export { ChatInput } from "./chat-input";
-export { ChatMessage } from "./chat-message";
-export { ChatThread } from "./chat-thread";
