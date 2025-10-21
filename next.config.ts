86d4d254b8fbe88454009401d28c1ee5385122ff
somtonnalue <leroynnalue65@gmail.com>
2025-10-21 02:10:22 +0100

    rollback next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@prisma/client'],
-  // Ensure Prisma client is bundled correctly
-  webpack: (config) => {
-    config.externals.push('@prisma/client');
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
